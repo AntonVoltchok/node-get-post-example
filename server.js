3c21f05515a28c1d2fcde26eb2d3b5a9c3c4690e
Anton Voltchok <antonvoltchok1@gmail.com>
2019-01-05 19:02:58 -0500

    updated eslintrc to use jest, moved side effect free functions into pureFunctions.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 
-const app = express();
-
 const bodyParser = require('body-parser');
 
 const request = require('superagent');
+
+const { getOperationResult, checkForUnsafeNumbers, logPostStatus } = require('./utils/pureFunctions');
+
+const app = express();
 
 app.use(bodyParser.json());
 
@@ -13,25 +15,6 @@ const port = process.env.PORT || 8080;
 const server = app.listen(port, () => {
   console.log('App listening on port', server.address().port);
 });
-
-const getOperationResult = ({ operation, left, right }) => {
-  console.log(`operation: ${operation} 1st val ${left}, 2nd val ${right} `);
-  switch (operation) {
-    case 'addition':
-      return left + right;
-    case 'subtraction':
-      return left - right;
-    case 'multiplication':
-      return left * right;
-    case 'division':
-      return left / right;
-    case 'remainder':
-      return left % right;
-    default:
-      return console.warn('MISSING / WRONG OPERATION PASSED IN');
-  }
-};
-
 
 const startNewTask = (cb) => {
   console.log('| NEW TASK -------------------');
@@ -44,14 +27,10 @@ const startNewTask = (cb) => {
         const {
           id, operation, left, right,
         } = res.body;
+        console.log('RES RES RES RES RES RES RES', res);
         const result = getOperationResult({ operation, left, right });
         console.log('the answer is:', result);
-        /*
-        Not necessarily an error with the api, but I noticed sometimes on
-        multiplication and addition the result is too large and isn't a safe number
-        */
-        const maxSafeVal = 9007199254740992;
-        if (result > maxSafeVal || result < -maxSafeVal) console.warn('WARNING RESULT IS NOT A SAFE NUMBER');
+        checkForUnsafeNumbers(result);
         cb({ id, result });
       }
     });
@@ -64,25 +43,12 @@ const checkAnswer = (errorCallback, cb, answer) => {
     .then(errorCallback, cb);
 };
 
-const logStatus = (status) => {
-  switch (status) {
-    case 200:
-      return console.log(`status ${status} - success: the answer is correct`);
-    case 400:
-      return console.log(`status ${status} - wrong value/no ID specified/value is invalid`);
-    case 500:
-      return console.log(`status ${status} - ID cannot be found`);
-    default:
-      return console.warn('status is not 200/400/500', status);
-  }
-};
-
-const startNewTaskAndValidate = () => startNewTask((obj) => {
+const startNewTaskAndValidate = () => startNewTask((answerCallback) => {
   console.log('... ... ... ... checking answer |');
   checkAnswer(
-    err => logStatus(err.status),
-    res => logStatus(res.status),
-    obj,
+    err => logPostStatus(err.status),
+    res => logPostStatus(res.status),
+    answerCallback,
   );
 });
 
---
+++ utils/pureFunctions.js
@@ -0,0 +1,43 @@
+const getOperationResult = ({ operation, left, right }) => {
+  console.log(`operation: ${operation} 1st val ${left}, 2nd val ${right} `);
+  switch (operation) {
+    case 'addition':
+      return left + right;
+    case 'subtraction':
+      return left - right;
+    case 'multiplication':
+      return left * right;
+    case 'division':
+      return left / right;
+    case 'remainder':
+      return left % right;
+    default:
+      return console.warn('MISSING / WRONG OPERATION PASSED IN');
+  }
+};
+
+const checkForUnsafeNumbers = (result) => {
+  // Not necessarily an error with the api, but I noticed sometimes on
+  // multiplication and addition the result is too large and isn't a safe number
+  const maxSafeVal = 9007199254740992;
+  if (result > maxSafeVal || result < -maxSafeVal) console.warn('WARNING RESULT IS NOT A SAFE NUMBER');
+};
+
+const logPostStatus = (status) => {
+  switch (status) {
+    case 200:
+      return console.log(`status ${status} - success: the answer is correct`);
+    case 400:
+      return console.log(`status ${status} - wrong value/no ID specified/value is invalid`);
+    case 500:
+      return console.log(`status ${status} - ID cannot be found`);
+    default:
+      return console.warn('status is not 200/400/500', status);
+  }
+};
+
+module.exports = {
+  getOperationResult,
+  checkForUnsafeNumbers,
+  logPostStatus,
+};
